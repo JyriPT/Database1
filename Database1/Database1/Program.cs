ab706c626ae9ef37c5e56b567d5bb059cc51b6d6
Jyri Turku <jyri.turku@student.saimia.fi>
2020-03-27 12:51:25 +0200

    Application now interacts with database.

--- Database1/Database1/Program.cs
+++ Database1/Database1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Database1.Data;
+using System.Linq;
 
 namespace Database1
 {
     class Program
     {
+        static private readonly PersondbContext context = new PersondbContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tulosta kaikki henkilöt:");
+            PrintAll();
+        }
+
+        static void PrintAll()
+        {
+            var persons = context.Person.ToList();
+
+            foreach (var p in persons)
+            {
+                Console.WriteLine($"Nimi: {p.Name}, Ikä: {p.Age}");
+            }
         }
     }
 }
